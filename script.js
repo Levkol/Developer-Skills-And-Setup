58b808c82567ef8a42271ada04e8276248a6661b
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-04-01 19:52:56 +0200

    Coding Challenge 1

--- script.js
+++ script.js
@@ -114,3 +114,58 @@ const calcTempAmplitudeBug = function (t1, t2) {
 const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
 // A) IDENTIFY
 console.log(amplitudeBug);
+
+//////////////////////////////////// Coding Challenge #1 /////////////////////////////////////////
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with the given temperatures. 
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Your tasks:
+1. Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console. Try it with both test datasets.
+2. Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+Test data:
+§ Data 1: [17, 21, 23]
+§ Data 2: [12, 5, -5, 0, 4]
+
+GOOD LUCK 😀
+*/
+
+// 1) Understanding the problem
+// - Array transformed to string, separated by ...
+// - What is the X days? Answer: index + 1
+
+// 2) Breaking up into sub-problems
+// - Transform array into string
+// - Transform each element to string with ºC
+// - Strings needs to contain day (index + 1)
+// - Add ... between elements and start and end of string
+// - Log string to console
+
+// const temp1 = [17, 21, 23];
+// const temp1 = [12, 5, -5, 0, 4];
+
+// const printForecast = function (arr) {
+//   console.log(
+//     `... ${temp1[0]}ºC in ${temp1.indexOf(temp1[0]) + 1} days ... ${
+//       temp1[1]
+//     }ºC in ${temp1.indexOf(temp1[1]) + 1} days ... ${temp1[2]}ºC in ${
+//       temp1.indexOf(temp1[2]) + 1
+//     } days ...`
+//   );
+// };
+
+// printForecast(temp1);
+
+// const temp1 = [17, 21, 23];
+const temp1 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = ``;
+  for (let i = 0; i < arr.length; i++) {
+    str = str + `${arr[i]}ºC in ${i + 1} days ... `;
+    // str += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  console.log(`...` + str);
+};
+printForecast(temp1);
